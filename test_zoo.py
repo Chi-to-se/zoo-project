768d5d0e809a2c6a5857430c3492ce9a75085a79
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2024-11-11 05:03:58 +0000

    Fixed major bug in zoo.py

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,48 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_ecp_case1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-5), "Invalid")
+
+    def test_ecp_case2(self):
+        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+
+    def test_ecp_case3(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_ecp_case4(self):
+        self.assertEqual(self.zoo.get_ticket_price(25), 150)
+
+    def test_ecp_case5(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100) 
+
+
+
+
+    def test_bva_case1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid")
+
+    def test_bva_case2(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_bva_case3(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_bva_case4(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_bva_case5(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_bva_case6(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_bva_case7(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_bva_case8(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: # bug fix
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: # bug fix
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: # bug fix
             return 150
         elif age >= 60:
             return 100
+        else: # <0 bug fix
+            return 'Invalid'
